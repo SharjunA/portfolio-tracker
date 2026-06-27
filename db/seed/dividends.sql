41feef7bf3d52e11f024a30f9c027c8230c23999
Sharjun A <sharjun.amanulla@gmail.com>
2026-06-27 19:04:01 +0530

    feat: seed db

---
+++ db/seed/dividends.sql
@@ -0,0 +1,15 @@
+SELECT * FROM dividends;
+
+INSERT INTO dividends (ticker, dividend_per_share, shares_held, record_date)
+VALUES -- 2025
+       ('RECLTD', 4.60, 3, '2025-10-26'),
+       ('ASHOKLEY', 1.00, 4, '2025-11-17'),
+       ('PFC', 3.65, 11, '2025-11-25'),
+       -- 2026
+       ('ITC', 6.50, 21, '2026-02-03'),
+       ('RECLTD', 4.60, 18, '2026-02-05'),
+       ('POWERGRID', 3.25, 4, '2026-02-08'),
+       ('PFC', 4.00, 19, '2026-02-19'),
+       ('RECLTD', 3.20, 18,  '2026-03-19'),
+       ('PFC', 3.25, 19,  '2026-03-22'),
+       ('ASHOKLEY', 2.50, 4, '2026-06-03');
